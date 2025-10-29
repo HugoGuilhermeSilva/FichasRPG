6714214253cb28b046632fa5a22d544531d3d121
Hugo Guilherme da Silva de Miranda <hugoguilherme332@gmail.com>
2025-10-29 08:45:04 -0300

    caixa de pericias funcionando

--- lib/screens/record_screen.dart
+++ lib/screens/record_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fichas/common/expertise_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fichas/common/attribute_fields.dart';
@@ -16,43 +17,79 @@ class RecordScreen extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
       ),
       body: SingleChildScrollView(
-        child: Column(
+        child: Wrap(
           children: [
-            const SizedBox(height: 8),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-              decoration: BoxDecoration(
-                  color: Colors.black,
-                  border: Border.all(
-                    color: Colors.deepPurple,
+            Column(
+            children: [
+              const SizedBox(height: 8),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    border: Border.all(
+                      color: Colors.deepPurple,
+                    ),
+                    borderRadius: BorderRadius.circular(16)),
+                width: 410,
+                height: 600,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Consumer<AttributesProvider>(
+                    builder: (context, provider, child) {
+                      return Column(
+                        children: attributeNames.map((name) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: AttributeFields(
+                              name: name,
+                              base: provider.baseControllers[name] ??
+                                  TextEditingController(),
+                              bonus: provider.bonusControllers[name] ??
+                                  TextEditingController(),
+                              total: provider.getAttributeTotalFor(name),
+                            ),
+                          );
+                        }).toList(),
+                      );
+                    },
                   ),
-                  borderRadius: BorderRadius.circular(16)),
-              width: 410,
-              height: 600,
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Consumer<AttributesProvider>(
-                  builder: (context, provider, child) {
-                    return Column(
-                      children: attributeNames.map((name) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: AttributeFields(
-                            name: name,
-                            base: provider.baseControllers[name] ??
-                                TextEditingController(),
-                            bonus: provider.bonusControllers[name] ??
-                                TextEditingController(),
-                            total: provider.getAttributeTotalFor(name),
-                          ),
-                        );
-                      }).toList(),
-                    );
-                  },
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 8),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    border: Border.all(
+                      color: Colors.deepPurple,
+                    ),
+                    borderRadius: BorderRadius.circular(16)),
+                width: 410,
+                height: 1400,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Consumer<AttributesProvider>(
+                    builder: (context, provider, child) {
+                      return Column(
+                        children: expertiseNames.map((name) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: ExpertiseFields(
+                              name: name,
+                              bonus: provider.expertiseBonusControllers[name] ??
+                                  TextEditingController(),
+                              total: provider.getExpertiseTotalFor(name),
+                            ),
+                          );
+                        }).toList(),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ]
         ),
       ),
     );
